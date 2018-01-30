1266d4ba0ee6a082e49b8b7871764e1eaf3d12d3
Hopson97 <mhopson@hotmail.co.uk>
2018-01-30 07:19:56 +0000

    begin work on front end of ias computer

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,18 @@
 #include "IASComputer.h"
 #include "IASAssembler.h"
 
+#include <SFML/Graphics.hpp>
+
+namespace {
+    void tryCloseWindow(sf::RenderWindow& window)
+    {
+        sf::Event e;
+        while (window.pollEvent(e)) {
+            if (e.type == sf::Event::Closed) window.close();
+        }
+    }
+}
+
 int main(int argc, char** argv)
 {
     std::string fileName = "tests/multiply.ias";
@@ -12,7 +24,23 @@ int main(int argc, char** argv)
     Memory memory = assembler.assembleFile(fileName.c_str());
 
     IASComputer computer(memory);
+
+    sf::RenderWindow window({1280, 720}, "8-bit IAS Computer");
+
+
+    while (window.isOpen()) {
+
+        window.clear();
+
+
+        window.display();
+        tryCloseWindow(window);
+    }
     computer.run();
+
+
+
+
 
     std::cout << "\nProgram terminated.\nPress any key to exit.\n";
     std::cin.ignore();std::cin.ignore();
